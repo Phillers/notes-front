625f492e5524b0d978164bee7dfffa9ccd88ea9a
Filip <phillers95@gmail.com>
2019-06-11 16:43:23 +0200

    first

--- src/config.js
+++ src/config.js
@@ -12,7 +12,8 @@ const dev = {
     USER_POOL_ID: "us-east-1_mZrqu9aG0",
     APP_CLIENT_ID: "5h5ftjf1ulodoqvr60hr7ga5th",
     IDENTITY_POOL_ID: "us-east-1:80422d3b-7294-4bad-90d4-aa17632dfe32"
-  }
+  },
+  STRIPE_KEY: "pk_test_ZUbhnJPiWlre3i0rn83Lp5il00jAWIH5Pv",
 };
 
 const prod = {
@@ -29,7 +30,8 @@ const prod = {
     USER_POOL_ID: "us-east-1_z0wSsVDHL",
     APP_CLIENT_ID: "3g4i5n19qe7i9dn4qdcfm9mmju",
     IDENTITY_POOL_ID: "us-east-1:8adaaa23-002e-4bd1-98be-10997eed7c9c"
-  }
+  },
+  STRIPE_KEY: "pk_test_ZUbhnJPiWlre3i0rn83Lp5il00jAWIH5Pv",
 };
 
 // Default to dev if not set
